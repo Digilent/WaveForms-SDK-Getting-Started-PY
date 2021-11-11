bc540d2c3055cde02b3999383d280b4ee85d52d5
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2021-11-11 14:52:41 +0200

    new static I/O - power supplies example

---
+++ test_static-supplies.py
@@ -0,0 +1,61 @@
+from WF_SDK import device, static, supplies       # import instruments
+
+from time import sleep                            # needed for delays
+
+"""-----------------------------------------------------------------------"""
+
+# connect to the device
+device_handle, device_name = device.open()
+
+# check for connection errors
+device.check_error(device_handle)
+
+"""-----------------------------------"""
+
+# start the positive supply
+supplies.switch(device_handle, device_name, True, True, False, 3.3, 0)
+
+# set maximum current
+if device_name == "Digital Discovery" or device_name == "Analog Discovery Pro 3X50":
+    static.set_current(device_handle, 16)
+
+# set all pins as output
+for index in range(16):
+    static.set_mode(device_handle, index, True)
+
+try:
+    while True:
+        # repeat
+        mask = 1
+        while mask < 0x10000:
+            # go through possible states
+            for index in range(16):
+                # set the state of every DIO channel
+                static.set_state(device_handle, index, not(mask & pow(2, index)))
+            sleep(0.1)  # delay
+            mask <<= 1  # switch mask
+
+        while mask > 1:
+            # go through possible states backward
+            mask >>= 1  # switch mask
+            for index in range(16):
+                # set the state of every DIO channel
+                static.set_state(device_handle, index, not(mask & pow(2, index)))
+            sleep(0.1)  # delay
+
+except KeyboardInterrupt:
+    # stop if Ctrl+C is pressed
+    pass
+
+finally:
+    # stop the static I/O
+    static.close(device_handle)
+
+    # stop and reset the power supplies
+    supplies.switch(device_handle, device_name, False, False, False, 0, 0)
+    supplies.close(device_handle)
+
+    """-----------------------------------"""
+
+    # close the connection
+    device.close(device_handle)
