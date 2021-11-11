91b5e755615b0c48f7ed065d4e24db0fe5765017
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2021-11-11 14:52:02 +0200

    power supplies wrapper function to handle different devices

--- WF_SDK/supplies.py
+++ WF_SDK/supplies.py
@@ -87,6 +87,28 @@ def switch_digital(device_handle, master_state, voltage):
 
 """-----------------------------------------------------------------------"""
 
+def switch(device_handle, device_name, master_state, positive_state, negative_state, positive_voltage, negative_voltage):
+    """
+        turn the power supplies on/off
+
+        parameters: - device handle
+                    - device name
+                    - master switch - True = on, False = off
+                    - positive supply switch - True = on, False = off
+                    - negative supply switch - True = on, False = off
+                    - positive supply voltage in Volts
+                    - negative supply voltage in Volts
+    """
+    if device_name == "Analog Discovery":
+        switch_fixed(device_handle, master_state, positive_state, negative_state)
+    elif device_name == "Analog Discovery 2" or device_name == "Analog Discovery Studio":
+        switch_variable(device_handle, master_state, positive_state, negative_state, positive_voltage, negative_voltage)
+    elif device_name == "Digital Discovery" or device_name == "Analog Discovery Pro 3X50":
+        switch_digital(device_handle, master_state, positive_voltage)
+    return
+
+"""-----------------------------------------------------------------------"""
+
 def close(device_handle):
     """
         reset the supplies
