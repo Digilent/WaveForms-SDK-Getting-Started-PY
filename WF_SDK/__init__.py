8ea94925fc8f233ca08734d69b202c9b823bc5ff
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2021-11-26 15:32:07 +0200

    correcting imports

--- WF_SDK/__init__.py
+++ WF_SDK/__init__.py
@@ -2,13 +2,13 @@
     This module realizes communication with Digilent Test & Measurement devices
 """
 
-import device
-import scope
-import wavegen
-import supplies
-import logic
-import pattern
-import static
-import protocol.uart
-import protocol.spi
-import protocol.i2c
+import WF_SDK.device
+import WF_SDK.scope
+import WF_SDK.wavegen
+import WF_SDK.supplies
+import WF_SDK.logic
+import WF_SDK.pattern
+import WF_SDK.static
+import WF_SDK.protocol.uart
+import WF_SDK.protocol.spi
+import WF_SDK.protocol.i2c
