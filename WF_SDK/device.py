a97f5883e4e9fd9f7fe9b110a54f18952feb1c52
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2021-12-01 17:23:29 +0200

    adding unused functions

--- WF_SDK/device.py
+++ WF_SDK/device.py
@@ -45,6 +45,21 @@ import dwfconstants as constants
 
 """-----------------------------------------------------------------------"""
 
+"""
+def open():
+    '''
+        open the first available device
+    '''
+    # this is the device handle - it will be used by all functions to "address" the connected device
+    device_handle = ctypes.c_int()
+
+    # connect to the first available device
+    dwf.FDwfDeviceOpen(ctypes.c_int(-1), ctypes.byref(device_handle))
+    return device_handle
+"""
+
+"""-----------------------------------------------------------------------"""
+
 def open(device=None):
     """
         open a specific device
