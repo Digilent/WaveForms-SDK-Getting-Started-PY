154a99261a20b17c0cb92b232db2e5b7d75458d0
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2021-12-01 17:15:14 +0200

    importing dwfconstants from the installation path

--- WF_SDK/device.py
+++ WF_SDK/device.py
@@ -1,5 +1,6 @@
 """ DEVICE CONTROL FUNCTIONS: open, check_error, close """
 
+"""
 import ctypes                            # import the C compatible data types
 import WF_SDK.dwfconstants as constants  # import every constant
 from sys import platform                 # this is needed to check the OS type
@@ -14,6 +15,33 @@ elif platform.startswith("darwin"):
 else:
     # on Linux
     dwf = ctypes.cdll.LoadLibrary("libdwf.so")
+
+"""
+
+"""-----------------------------------------------------------------------"""
+
+import ctypes                     # import the C compatible data types
+from sys import platform, path    # this is needed to check the OS type and get the PATH
+from os import sep                # OS specific file path separators
+
+# load the dynamic library, get constants path (the path is OS specific)
+if platform.startswith("win"):
+    # on Windows
+    dwf = ctypes.cdll.dwf
+    constants_path = "C:" + sep + "Program Files (x86)" + sep + "Digilent" + sep + "WaveFormsSDK" + sep + "samples" + sep + "py"
+elif platform.startswith("darwin"):
+    # on macOS
+    lib_path = sep + "Library" + sep + "Frameworks" + sep + "dwf.framework" + sep + "dwf"
+    dwf = ctypes.cdll.LoadLibrary(lib_path)
+    constants_path = sep + "Applications" + sep + "WaveForms.app" + sep + "Contents" + sep + "Resources" + sep + "SDK" + sep + "samples" + sep + "py"
+else:
+    # on Linux
+    dwf = ctypes.cdll.LoadLibrary("libdwf.so")
+    constants_path = sep + "usr" + sep + "share" + sep + "digilent" + sep + "waveforms" + sep + "samples" + sep + "py"
+
+# import constants
+path.append(constants_path)
+import dwfconstants as constants
 
 """-----------------------------------------------------------------------"""
 
--- WF_SDK/logic.py
+++ WF_SDK/logic.py
@@ -1,19 +1,27 @@
 """ LOGIC ANALYZER CONTROL FUNCTIONS: open, trigger, record, close """
 
-import ctypes                            # import the C compatible data types
-import WF_SDK.dwfconstants as constants  # import every constant
-from sys import platform                 # this is needed to check the OS type
+import ctypes                     # import the C compatible data types
+from sys import platform, path    # this is needed to check the OS type and get the PATH
+from os import sep                # OS specific file path separators
 
-# load the dynamic library (the path is OS specific)
+# load the dynamic library, get constants path (the path is OS specific)
 if platform.startswith("win"):
     # on Windows
     dwf = ctypes.cdll.dwf
+    constants_path = "C:" + sep + "Program Files (x86)" + sep + "Digilent" + sep + "WaveFormsSDK" + sep + "samples" + sep + "py"
 elif platform.startswith("darwin"):
     # on macOS
-    dwf = ctypes.cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf")
+    lib_path = sep + "Library" + sep + "Frameworks" + sep + "dwf.framework" + sep + "dwf"
+    dwf = ctypes.cdll.LoadLibrary(lib_path)
+    constants_path = sep + "Applications" + sep + "WaveForms.app" + sep + "Contents" + sep + "Resources" + sep + "SDK" + sep + "samples" + sep + "py"
 else:
     # on Linux
     dwf = ctypes.cdll.LoadLibrary("libdwf.so")
+    constants_path = sep + "usr" + sep + "share" + sep + "digilent" + sep + "waveforms" + sep + "samples" + sep + "py"
+
+# import constants
+path.append(constants_path)
+import dwfconstants as constants
 
 """-----------------------------------------------------------------------"""
 
