a8d999cec732f9b68e65464b1b576e1c6cd071fc
Álmos Veres-Vitályos <93523424+vvAlmos@users.noreply.github.com>
2022-01-20 16:24:02 +0200

    create installer

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+   name = "WF_SDK",
+   version = "1.0",
+   description = "This module realizes communication with Digilent Test & Measurement devices",
+   license = "MIT",
+   long_description = long_description,
+   author = "Digilent Inc.",
+   author_email = "almos.veres-vitalyos@digilent.ro",
+   url = "https://digilent.com/reference/test-and-measurement/guides/waveforms-sdk-getting-started",
+   packages = ["WF_SDK", "WF_SDK.protocol"],
+)
