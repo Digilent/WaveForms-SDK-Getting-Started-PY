819b0d679ffe7a6adb99bfc5f626ced0e956462c
Álmos Veres-Vitályos <93523424+vvAlmos@users.noreply.github.com>
2022-01-20 16:19:59 +0200

    update initializer

--- WF_SDK/__init__.py
+++ WF_SDK/__init__.py
@@ -1,15 +1,13 @@
 """
-    This module realizes communication with Digilent Test & Measurement devices
+This module realizes communication with Digilent Test & Measurement devices
 """
 
-import WF_SDK.device
-import WF_SDK.scope
-import WF_SDK.wavegen
-import WF_SDK.supplies
-import WF_SDK.dmm
-import WF_SDK.logic
-import WF_SDK.pattern
-import WF_SDK.static
-import WF_SDK.protocol.uart
-import WF_SDK.protocol.spi
-import WF_SDK.protocol.i2c
+from WF_SDK import device
+from WF_SDK import scope
+from WF_SDK import wavegen
+from WF_SDK import supplies
+from WF_SDK import dmm
+from WF_SDK import logic
+from WF_SDK import pattern
+from WF_SDK import static
+from WF_SDK import protocol
