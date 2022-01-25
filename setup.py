7c142a096677296453feddb0f103bacc17546698
Álmos Veres-Vitályos <93523424+vvAlmos@users.noreply.github.com>
2022-01-25 17:00:31 +0200

    changing WF_SDK to WFSDK

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-   name = "WF_SDK",
+   name = "WFSDK",
    version = "1.0",
    description = "This module realizes communication with Digilent Test & Measurement devices",
    license = "MIT",
@@ -12,5 +12,5 @@ setup(
    author = "Digilent Inc.",
    author_email = "almos.veres-vitalyos@digilent.ro",
    url = "https://digilent.com/reference/test-and-measurement/guides/waveforms-sdk-getting-started",
-   packages = ["WF_SDK", "WF_SDK.protocol"],
+   packages = ["WFSDK", "WFSDK.protocol"],
 )
