19369ca47727c5c1a910a9b6d07995d3c352f369
Almos Veres-Vitalyos <almos.veres-vitalyos@ni.com>
2022-09-05 10:49:29 +0300

    update example

--- test_logic-pattern.py
+++ test_logic-pattern.py
@@ -1,8 +1,12 @@
 from WF_SDK import device, logic, pattern   # import instruments
 
 import matplotlib.pyplot as plt   # needed for plotting
+from time import sleep            # needed for delays
 
 """-----------------------------------------------------------------------"""
+
+DIO_IN = 0
+DIO_OUT = 0
 
 # connect to the device
 device_data = device.open()
@@ -18,14 +22,26 @@ logic.open(device_data)
 # set up triggering on DIO0 falling edge
 logic.trigger(device_data, enable=True, channel=0, rising_edge=False)
 
-# generate a 100KHz PWM signal with 30% duty cycle on DIO0
-pattern.generate(device_data, channel=0, function=pattern.function.pulse, frequency=100e03, duty_cycle=30)
+# generate a 100KHz PWM signal with 30% duty cycle on a DIO channel
+pattern.generate(device_data, channel=DIO_OUT, function=pattern.function.pulse, frequency=100e03, duty_cycle=30)
 
-# record a logic signal on DIO0
-buffer, time = logic.record(device_data, channel=0)
+sleep(1)    # wait 1 second
+
+# record a logic signal on a DIO channel
+buffer = logic.record(device_data, channel=DIO_IN)
+
+# limit displayed data size
+length = len(buffer)
+if length > 10000:
+    length = 10000
+buffer = buffer[0:length]
+
+# generate buffer for time moments
+time = []
+for index in range(length):
+    time.append(index * 1e06 / logic.data.sampling_frequency)   # convert time to μs
 
 # plot
-time = [moment * 1e06 for moment in time]   # convert time to μs
 plt.plot(time, buffer)
 plt.xlabel("time [μs]")
 plt.ylabel("logic value")
--- test_scope-wavegen.py
+++ test_scope-wavegen.py
@@ -1,6 +1,7 @@
-from WF_SDK import device, scope, wavegen   # import instruments
+from WF_SDK import device, scope, wavegen, tools   # import instruments
 
 import matplotlib.pyplot as plt   # needed for plotting
+from time import sleep            # needed for delays
 
 """-----------------------------------------------------------------------"""
 
@@ -24,15 +25,49 @@ if device_data.name != "Digital Discovery":
     # generate a 10KHz sine signal with 2V amplitude on channel 1
     wavegen.generate(device_data, channel=1, function=wavegen.function.sine, offset=0, frequency=10e03, amplitude=2)
 
+    sleep(1)    # wait 1 second
+
     # record data with the scopeon channel 1
-    buffer, time = scope.record(device_data, channel=1)
+    buffer = scope.record(device_data, channel=1)
+
+    # limit displayed data size
+    length = len(buffer)
+    if length > 10000:
+        length = 10000
+    buffer = buffer[0:length]
+
+    # generate buffer for time moments
+    time = []
+    for index in range(len(buffer)):
+        time.append(index * 1e03 / scope.data.sampling_frequency)   # convert time to ms
 
     # plot
-    time = [moment * 1e03 for moment in time]   # convert time to ms
     plt.plot(time, buffer)
     plt.xlabel("time [ms]")
     plt.ylabel("voltage [V]")
     plt.show()
+
+    """-----------------------------------"""
+
+    # compute the spectrum from 0Hz to 100KHz
+    start_frequency = 0
+    stop_frequency = 100e03
+    spectrum = tools.spectrum(buffer, tools.window.flat_top, scope.data.sampling_frequency, start_frequency, stop_frequency)
+
+    # calculate frequency domain data
+    frequency = []
+    length = len(spectrum)
+    step = (stop_frequency - start_frequency) / (length - 1)
+    for index in range(length):
+        frequency.append((start_frequency + index * step) / 1e06)   # convert frequency in MHz
+    
+    # plot
+    plt.plot(frequency, spectrum)
+    plt.xlabel("frequency [MHz]")
+    plt.ylabel("magnitude [dBV]")
+    plt.show()
+
+    """-----------------------------------"""
 
     # reset the scope
     scope.close(device_data)
